c6a4d182518d39bc154371bcb7975c7fc79e7754
João Victor <joao.silvassenai@gmail.com>
2025-04-25 11:11:30 -0300

    Use .length para contar 🎯

--- Array_atividades/atividade09.js
+++ Array_atividades/atividade09.js
@@ -1,1 +1,8 @@
-let frutas = ['uva' , 'banana' , 'maça' , 'manga' , 'melancia' , 'mamao'];
+// Use .length para contar 🎯
+]let frutas = ['uva' , 'banana' , 'maça' , 'manga' , 'melancia' , 'mamao'];
+let quant = frutas.length
+ 
+console.log("aqui temos " + quant + " frutas no total sendo elas:")
+for(let i = 0 ; i< quant ;i++){
+        console.log(frutas[i]);
+    }
