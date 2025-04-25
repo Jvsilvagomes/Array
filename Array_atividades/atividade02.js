13d57b6c23da604c1971aab921b2196bde02fbe9
João Victor <joao.silvassenai@gmail.com>
2025-04-25 09:34:47 -0300

    Mostre sua playlist favorita 🎵

---
+++ Array_atividades/atividade02.js
@@ -0,0 +1,7 @@
+// mostre sua playlist favorita
+let playlist = ['imagine' , '505' , 'i gotta feeling' , 'champions'];
+let quantidade = playlist.length;
+
+for (let i = 0; i < quantidade; i++){
+    console.log(playlist[i])
+ }
